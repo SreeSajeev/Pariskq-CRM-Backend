60ca605a2097c34dd38881cdccec010cdbf79b1b
SreeSajeev <sreeparvathysajeev@gmail.com>
2026-01-10 13:09:09 +0530

    backed working fixed

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { createClient } from '@supabase/supabase-js';
+
+if (!process.env.SUPABASE_URL) {
+  throw new Error('SUPABASE_URL missing in env');
+}
+
+if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
+  throw new Error('SUPABASE_SERVICE_ROLE_KEY missing in env');
+}
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
